bed262ae03fc2fe38b9a5f9fbccbff52944fc0b8
Geoff George <geoffgeorgethommana@gmail.com>
2023-09-23 22:34:56 +0530

    avatar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,15 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png';
 import './navbar.scss';
 import search from '../assets/search-solid.svg';
+import Avatar from './avatar';
 
 const Navbar = () => {
+
+  const User="nues";
+
+  const handleLogout=()=>{
+
+  }
   return (
     <div className='navbar'>
         <Link to={'/'} className='nav-item nav-btn'  > 
@@ -17,10 +24,39 @@ const Navbar = () => {
 
         <form>
           <input type='text' placeholder='...search'></input>
-          <img src={search} alt='search' width='18'></img>
+          <img src={search} alt='search' width='18' className='search-icon'></img>
         </form>
+
+        <div className="navbar-2">
+          {User === null ? (
+            <Link to="/Auth" className="nav-item nav-links">
+              Log in
+            </Link>
+          ) : (
+            <>
+              <Avatar
+                backgroundColor="#009dff"
+                px="10px"
+                py="7px"
+                borderRadius="50%"
+                color="white"
+              >
+                <Link
+                  to={`/Users/${User?.result?._id}`}
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  {/* {User.result.name.charAt(0).toUpperCase()} */}
+                  M
+                </Link>
+              </Avatar>
+              <button className="nav-item nav-links" onClick={handleLogout}>
+                Log out
+              </button>
+            </>
+          )}
+        </div>
         
-        <button>Log In</button>
+        
     </div>
   )
 }
