53d947a2886c26eca977d0768a3a63348b096568
Geoff George <geoffgeorgethommana@gmail.com>
2023-09-25 20:02:46 +0530

    css

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,31 +1,110 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+import "./auth.scss";
+import icon from "../assets/icon.png";
+import AboutAuth from "./aboutauth";
+// import { signup, login } from "../../actions/auth";
 const Auth = () => {
+  const [isSignup, setIsSignup] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const[isSignUp,setisSignup]=useState(false);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+  const handleSwitch = () => {
+    setIsSignup(!isSignup);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email && !password) {
+      alert("Enter email and password");
+    }
+    if (isSignup) {
+      if (!name) {
+        alert("Enter a name to continue");
+      }
+    //   dispatch(signup({ name, email, password }, navigate));
+    } else {
+    //   dispatch(login({ email, password }, navigate));
+    }
+  };
+
   return (
-    <div className='auth-section'>
+    <section className="auth-section">
+      {isSignup && <AboutAuth />}
+      <div className="auth-container-2">
+        <img src={icon} alt="stack overflow" className="login-logo" />
+        <form onSubmit={handleSubmit}>
+          {isSignup && (
+            <label htmlFor="name">
+              <h4>Display Name</h4>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </label>
+          )}
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h4>Password</h4>
+              {!isSignup && (
+                <p style={{ color: "#007ac6", fontSize: "13px" }}>
+                  forgot password?
+                </p>
+              )}
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <button type="submit" className="auth-btn">
+            {isSignup ? "Sign up" : "Log in"}
+          </button>
+        </form>
+        <p>
+          {isSignup ? "Already have an account?" : "Don't have an account?"}
+          <button
+            type="button"
+            className="handle-switch-btn"
+            onClick={handleSwitch}
+          >
+            {isSignup ? "Log in" : "sign up"}
+          </button>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-        <div className='auth-container'>
-            
-            <form>
-                <label htmlFor='email'>
-                    <h4>Email</h4>
-                    <input type='email'></input>
-                   
-                </label>
-                <label>
-                    <h4>Password</h4>
-                    <input type='password'></input>
-                </label>
-
-                <button className='auth-btn'>{isSignUp?"Login":"SignUp"}</button>
-                
-            </form>
-        </div>
-
-    </div>
-  )
-}
-
-export default Auth
+export default Auth;
