7d3f4214c07a240041d4059a7a7f67c99be77ea2
Geoff George <geoffgeorgethommana@gmail.com>
2023-09-25 23:45:25 +0530

    rightSidebar

--- src/components/rightSidebar.jsx
+++ src/components/rightSidebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Widget from './widget';
+import WidgetTags from './widgetTags';
+import './rightSidebar.scss';
 
 const RightSidebar = () => {
   return (
-    <div>R</div>
+    <aside className='right-sidebar'>
+        <Widget/>
+        <WidgetTags/>
+    </aside>
   )
 }
 
