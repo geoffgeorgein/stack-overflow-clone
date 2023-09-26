b2678377ab640945866bbacba1374f91eb00bfb7
Geoff George <geoffgeorgethommana@gmail.com>
2023-09-26 23:31:32 +0530

    question

---
+++ src/pages/askQuestion.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AskQuestion = () => {
+
+    const user=null;
+  return (
+    <div>
+
+    {
+        user===null?
+        alert("LOgin  or SignUP")
+        :
+        <div>
+            
+        </div>
+    }
+
+    </div>
+  )
+}
+
+export default AskQuestion 
